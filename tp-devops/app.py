0afea97eb973bf9f70401a96ce348edc25d0800b
taiwo <fouadtaiwo@gmail.com>
2026-03-31 15:07:02 +0200

    version 1.1

--- tp-devops/app.py
+++ tp-devops/app.py
@@ -4,7 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello DevOps"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    return "Hello DevOps Version 2 BUGGEE"
