42e86e87090afeaa3959f7372438a2d2f6f92b78
taiwo <fouadtaiwo@gmail.com>
2026-03-31 10:44:55 +0200

    add Dockerfile and Flask app

--- tp-devops/app.py
+++ tp-devops/app.py
@@ -1,3 +1,11 @@
-#Fichier app.py
-print("Hello DevOps")
-print("Version 2")
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    message = os.getenv("MESSAGE", "Hello Default")
+    return message
+
+app.run(host="0.0.0.0", port=5000)
